85e776f05e9d0e92c4d33641d1a06025789b6838
Daniel Hons <daniel.hons@intradesys.com>
2017-04-15 02:56:07 +0200

    neo4j included. build stable

---
+++ src/main/java/graphEndpoint/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package graphEndpoint.repository;
+
+/**
+ * Created by Daniel on 15.04.2017.
+ */
+public class CustomerRepository {
+}
---
+++ src/test/java/graphEndpoint/MainServerTest.java
@@ -0,0 +1,7 @@
+package graphEndpoint;
+
+/**
+ * Created by Daniel on 14.04.2017.
+ */
+public class MainServerTest {
+}
