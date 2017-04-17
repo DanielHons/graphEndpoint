18b691da2d52aa085f2aed7d52f9dca464b4f8df
Daniel Hons <daniel.hons@intradesys.com>
2017-04-17 13:56:01 +0200

    baseline before re-adding security including tests (so far everything working)

--- src/main/java/graphEndpoint/security/View.java
+++
@@ -1,7 +0,0 @@
-package graphEndpoint.security;
-
-/**
- * Created by Daniel on 15.04.2017.
- */
-public class View {
-}
---
+++ src/test/java/graphEndpoint/dataConnection/controller/AuthenticationTest.java
@@ -0,0 +1,7 @@
+package graphEndpoint.dataConnection.controller;
+
+/**
+ * Created by Daniel Hons on 15.04.2017.
+ */
+public class AuthenticationTest {
+}
